cba43d7436cf2fa5fce79871070b444a83b8a97c
Fernando Poli <fernandoandrespoli@gmail.com>
2020-08-25 15:08:29 -0300

    master : Adding new changes.

--- single-artist.php
+++ single-artist.php
@@ -5,39 +5,52 @@
 
 ?>
 <?php get_header(); ?>
-<main id="content">
+<main id="content" class="mt-16 pr-4 lg:pr-0 lg:pl-0 pl-4">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<header class="entry-header">
-				<h1 class="entry-title">
-					<a href="<?php the_permalink(); ?>">
-						<?php the_title(); ?>
+			<h1 class="uppercase mb-12">
+				<a href="<?php echo get_home_url(); ?>" title="Back to Homepage" class="mr-2">
+					<i class="icn-back"></i>
+				</a>
+				<?php the_title(); ?>
+			</h1>
+			<div class="bio__wrapper flex mb-12 lg:ml-8">
+				<div class="w-auto">
+					<div class="bio--image__wrapper" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)"></div>
+				</div>
+				<div class="bio--info__wrapper ml-6 flex flex-col">
+					<h2 class="mb-5 ">BIO</h2>
+					<div class="info--wrapper flex-grow">
+						<?php echo get_field('biography'); ?>
+					</div>
+					<?php if (get_the_title($artist_website->ID)); ?>
+					<a href="<?php ?>" title="Visit <?php echo get_the_title($artist_website->ID); ?> Website" class="btn uppercase">
+						View Artist Website
 					</a>
-				</h1>
-				<h2>BIO</h2>
-				<p>
-					<?php echo get_field('biography'); ?>
-				</p>
-				<?php the_post_thumbnail(); ?>
-				<h2>ART</h2>
-				<p>
+					<? endif; ?>
+				</div>
+			</div>
+			<div class="bio__wrapper lg:ml-8">
+				<h2 class="mb-6">ART</h2>
+				<div class="info--wrapper mb-16">
 					<?php echo get_field('art'); ?>
-				</p>
-				<h3 class="text-uppercase">Other Available Painting from this artist</h3>
+				</div>
+				<h3 class="uppercase mb-8"><em>Other Available Painting from this artist</em></h3>
 				<?php $pieces = get_field('piece'); ?>
 				<?php if ($pieces) : ?>
-					<div class="flex flex-wrap painting__wrapper space-x-6">
+					<div class="grid grid-cols-6 gap-6 painting__wrapper">
 						<?php foreach ($pieces as $piece) : ?>
-							<div class="w-12 lg:w-3/12 painting--item__wrapper">
+							<div class="painting--item__wrapper">
 								<a href="<?php echo get_permalink($piece->ID); ?>">
 
-									<div class="painting__item" style="background-image: url(<?php echo get_the_post_thumbnail_url($piece->ID, 'thumbnail'); ?>)"></div>
-									<?php echo get_the_title($piece->ID); ?>
+									<div class="painting__item mb-4" style="background-image: url(<?php echo get_the_post_thumbnail_url($piece->ID, 'thumbnail'); ?>)"></div>
+									<h4><?php echo get_the_title($piece->ID); ?></h4>
+									<h5><?php echo get_the_title($artist->ID); ?></h5>
 								</a>
 							</div>
 						<?php endforeach; ?>
 					</div>
 				<?php endif; ?>
-			</header>
+			</div>
 	<?php endwhile;
 	endif; ?>
 </main>
