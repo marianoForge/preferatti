9088a11518542f3ea5596a3327a1f2e683cd2cd7
Fernando Poli <fernandoandrespoli@gmail.com>
2020-08-25 16:44:32 -0300

    master : Adding new changes.

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <header id="header">
-    <nav id="main-menu-options" class="mb-4 lg:mb-6">
+    <nav id="main-menu-options" class="mb-4 lg:mb-10">
         <?php wp_nav_menu(array('theme_location' => 'main-menu-options')); ?>
     </nav>
     <nav id="menu-artists">
